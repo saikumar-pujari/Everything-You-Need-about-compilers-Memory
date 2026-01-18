55c68db8fde46986cc7bcd11e6aa7be8bbcf03ab
saikumar <159767386+saikumar-pujari@users.noreply.github.com>
2026-01-18 23:05:52 +0530

    Add BASH scripting guide to hello.sh
    Updated the hello.sh script to include a comprehensive BASH scripting guide with various examples and explanations.

--- hello.sh
+++ hello.sh
@@ -1,146 +1,406 @@
-#! /usr/bin/bash
-#this is a comment here
-# echo 'Skipper'
-# echo 'sandy'
-# echo 'niki ' #so this is a ccomment here!!
-# echo $BASH
-# echo $BASH_VERSION
-# echo $HOME
-# echo $PWD
+#!/usr/bin/bash
 
-# name=SKipper
-# echo  the name is $name #anything after # is variable!
-# val10=10
-# 10=10
-# 0=10
-# echo $val10
-# echo $10
-# echo $0
+# ========================================
+# BASH SCRIPTING GUIDE - GEN Z EDITION 🚀
+# ========================================
+
+# 1. COMMENTS & BASIC OUTPUT
+# ---------------------------
+# Anything after # is a comment - bash ignores it
+echo '=== 1. COMMENTS & BASIC OUTPUT ==='
+echo 'Skipper'  # This comment won't be executed
+echo 'sandy'
+echo 'niki'
 
 
-# -sp for password adn -p for same line -a for array
-# echo 'enter names:'
-# read -sp password name1 name2 name3 
-# echo the name is: $name1 $name2 $name3 
-
-# echo 'enter names'
-# read -a name
-# echo ${name[0]} ${name[1]}${name[2]}${name[4]}
-
-# echo 'name:'
-# read  
-# echo $REPLY       #reply autoreads the and prints direclty
+# 2. BUILT-IN VARIABLES
+# ---------------------
+echo ''
+echo '=== 2. BUILT-IN VARIABLES ==='
+echo "Bash path: $BASH"
+echo "Bash version: $BASH_VERSION"
+echo "Home directory: $HOME"
+echo "Current directory: $PWD"
 
 
-# echo $0 $1 $2 $3 '>echo $1 $2 $3 $4' 
+# 3. CUSTOM VARIABLES
+# -------------------
+echo ''
+echo '=== 3. CUSTOM VARIABLES ==='
+name="Skipper"
+echo "The name is: $name"
 
-# args=("$@") #using array
-# echo ${args[0]} ${args[1]} ${args[2]} ${args[4]}
-# echo $@ #prints all the argument inside the array
-# echo $# #no of arguments inside the array
+val10=10
+echo "Value: $val10"
 
+# WRONG WAYS (commented out because they cause errors):
+# 10=10        # ❌ Can't start with number
+# 0=10         # ❌ Can't start with number
+# echo $10     # ❌ This refers to command line argument #10, not variable
 
-
-# count=10
-# if [ $count > 9 ]
-# then
-#     echo 'condition is true'
-# fi
-
-# word=abc
-# if [ $word == 'abc' ]
-# then
-#     echo 'its same'
-# fi
-# if [ $word == 'abcdd' ]
-# then
-#     echo 'its same'
-# else
-#     echo 'condition is false'
-# fi
+echo "Script name: $0"  # $0 is the script name
 
 
-# cd /mnt/c/Users/<your partition name>/Desktop
-# cd /mnt/c/Users/saikumar/Desktop
+# 4. READING USER INPUT
+# ----------------------
+echo ''
+echo '=== 4. READING USER INPUT ==='
+
+# Basic read (uncomment to test):
+# echo 'Enter your name:'
+# read username
+# echo "Hey $username!"
+
+# Multiple inputs (uncomment to test):
+# echo 'Enter 3 names:'
+# read name1 name2 name3
+# echo "Names are: $name1, $name2, $name3"
+
+# Silent password input with -sp flag (uncomment to test):
+# echo 'Enter password:'
+# read -sp password
+# echo ''
+# echo "Password saved!"
+
+# Array input with -a flag (uncomment to test):
+# echo 'Enter multiple names:'
+# read -a names
+# echo "First: ${names[0]}, Second: ${names[1]}, Third: ${names[2]}"
+
+# Auto-read with REPLY (uncomment to test):
+# echo 'Say something:'
+# read
+# echo "You said: $REPLY"
 
 
-# echo $(hostname)
-# echo $(unname -r)
-# echo $(uptime -p)
-# echo $(whoami)
+# 5. COMMAND LINE ARGUMENTS
+# --------------------------
+echo ''
+echo '=== 5. COMMAND LINE ARGUMENTS ==='
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Third argument: $3"
+echo "All arguments: $@"
+echo "Number of arguments: $#"
 
-# $1 refers to the first argument, $2 to the second, and so on.
-# echo “Hello, $1!”
-# echo “You are using the $2 theme.”
+# Using arrays for arguments
+args=("$@")
+echo "Using array - First arg: ${args[0]}, Second arg: ${args[1]}"
 
-# echo -e 'file name:\c' #-e to keep the curson on same line
+
+# 6. IF STATEMENTS - NUMBER COMPARISONS
+# --------------------------------------
+echo ''
+echo '=== 6. IF STATEMENTS - NUMBERS ==='
+
+count=10
+if [ $count -gt 9 ]  # -gt means "greater than"
+then
+    echo 'Count is greater than 9'
+fi
+
+# Comparison operators:
+# -gt : greater than
+# -lt : less than
+# -eq : equal to
+# -ne : not equal to
+# -ge : greater than or equal to
+# -le : less than or equal to
+
+
+# 7. IF STATEMENTS - STRING COMPARISONS
+# --------------------------------------
+echo ''
+echo '=== 7. IF STATEMENTS - STRINGS ==='
+
+word="abc"
+if [ $word == "abc" ]
+then
+    echo 'Word matches abc'
+fi
+
+if [ $word == "abcdd" ]
+then
+    echo 'This wont print'
+else
+    echo 'Word does not match abcdd'
+fi
+
+
+# 8. SYSTEM COMMANDS
+# ------------------
+echo ''
+echo '=== 8. SYSTEM COMMANDS ==='
+echo "Hostname: $(hostname)"
+echo "Kernel: $(uname -r)"
+echo "Uptime: $(uptime -p)"
+echo "Current user: $(whoami)"
+
+
+# 9. FILE OPERATIONS
+# ------------------
+echo ''
+echo '=== 9. FILE OPERATIONS ==='
+
+# Example file check (uncomment to test):
+# echo -e 'Enter filename:\c'  # -e enables \c to keep cursor on same line
 # read file_name
 
-# if [ -e "$file_name" ]    #-e for file there or not and  -f for does file exisits or not! and -d for directory!!
+# if [ -e "$file_name" ]  # -e checks if file exists
 # then
 #     echo "$file_name found"
 # else
 #     echo "$file_name not found"
 # fi
 
-# if [ -f file_name ]    
-# then
-#     echo "$file_name exists"
-# else
-#     echo "$file_name not exists"
-# fi
+# File test operators:
+# -e : exists (file or directory)
+# -f : exists and is a regular file
+# -d : exists and is a directory
+# -s : exists and is not empty
+# -r : readable
+# -w : writable
+# -x : executable
 
-#-s checks wheter the file is empty ,-r for read ,-x for write nd -w for permission
 
-# if [ -f file_name ]    
-# then
-#     echo "$file_name empty"
-# else
-#     echo "$file_name not empty"
-# fi
+# 10. FILE CONTENT CHECK
+# ----------------------
+echo ''
+echo '=== 10. FILE CONTENT CHECK ==='
 
-# to add new data in exisiting data use cat >> and to overwrite use cat >
-#chmod to change permsiion for a file like read or write using -r or -w to add permisson do +r or +w
-
+# Example (uncomment to test):
 # if [ -f "$file_name" ]
 # then
-#     if [ ! -s "$file_name" ]
+#     if [ ! -s "$file_name" ]  # ! means NOT
 #     then
-#         echo 'no data here.to quit press cltr+c'
-#         cat >> "$file_name"
+#         echo 'File is empty. Adding data (Ctrl+C to quit):'
+#         cat >> "$file_name"  # >> appends to file
 #     else
-#         echo 'there is already some data present'
+#         echo 'File already has data'
 #     fi
 # else
-#     echo 'file does not exists'
+#     echo 'File does not exist'
 # fi
 
-# age=25
-# if [ "$age" -gt 18 ] && [ "$age" -lt 30 ] #Tip: -a  also can be used instead of &&
-# then
-#     echo 'using AND operation'
-# fi
+# File operations:
+# cat > file.txt   # Overwrite file
+# cat >> file.txt  # Append to file
+# chmod +r file    # Add read permission
+# chmod +w file    # Add write permission
+# chmod +x file    # Add execute permission
 
-# age_no=51
-# if  [ "$age_no" -gt 58 ] || [ "$age_no" -lt 50 ] #Tip: -l can also be unsed instead for ||
-# then
-#     echo 'using OR operation'
-# else
-#     echo 'using OP operation with false(else)'
-# fi
 
-# num1=10
-# num2=10
-# echo $(( num1 + num2 ))
-# echo $(( num1 - num2 ))
-# echo $(( num1 * num2 ))
-# echo $(( num1 / num2 ))
-# echo $(( num1 % num2 ))
-# echo $(expr $num1 + $num2)
+# 11. LOGICAL OPERATORS
+# ----------------------
+echo ''
+echo '=== 11. LOGICAL OPERATORS ==='
 
-# num1=10.0
-# num2=10.0
-# # echo $(( num1 + num2 )) #it will give error man!! as its a float not int
-# echo "20.0+3" |bc
+age=25
+if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]  # && means AND
+then
+    echo 'Age is between 18 and 30'
+fi
 
-# -l for math lib(man bc)
+# Alternative AND syntax:
+if [ "$age" -gt 18 -a "$age" -lt 30 ]  # -a also means AND
+then
+    echo 'Still between 18 and 30'
+fi
+
+age_no=51
+if [ "$age_no" -gt 58 ] || [ "$age_no" -lt 50 ]  # || means OR
+then
+    echo 'Age is either above 58 or below 50'
+else
+    echo 'Age is between 50 and 58'
+fi
+
+
+# 12. ARITHMETIC OPERATIONS
+# --------------------------
+echo ''
+echo '=== 12. ARITHMETIC OPERATIONS ==='
+
+num1=10
+num2=5
+
+echo "Addition: $(( num1 + num2 ))"
+echo "Subtraction: $(( num1 - num2 ))"
+echo "Multiplication: $(( num1 * num2 ))"
+echo "Division: $(( num1 / num2 ))"
+echo "Modulus: $(( num1 % num2 ))"
+
+# Alternative using expr:
+echo "Using expr - Addition: $(expr $num1 + $num2)"
+
+# Float calculations require bc (uncomment if bc is installed):
+# echo "Float calc: "
+# echo "20.5 + 3.2" | bc
+# echo "scale=2; 10/3" | bc -l  # -l loads math library
+
+
+# 13. CASE STATEMENTS
+# -------------------
+echo ''
+echo '=== 13. CASE STATEMENTS ==='
+
+vehicle=$1
+case $vehicle in
+    "car" )
+        echo "Rent of $vehicle is 100rs" ;;
+    "bike" )
+        echo "Rent of $vehicle is 50rs" ;;
+    "sai" )
+        echo "Vehicle is owned by $vehicle" ;;
+    * )  # * is like default case
+        echo "Unknown vehicle" ;;
+esac
+
+
+# 14. ARRAYS
+# ----------
+echo ''
+echo '=== 14. ARRAYS ==='
+
+os=('ubuntu' 'windows' 'linux')
+echo "All OS: ${os[@]}"
+echo "Second OS: ${os[1]}"
+echo "Array length: ${#os[@]}"
+
+# Modify array
+os[1]='win10'
+echo "After modification: ${os[@]}"
+
+# Remove element
+unset os[1]
+echo "After deletion: ${os[@]}"
+
+# String as array
+string="abcdefgh"
+echo "String: ${string[@]}"
+echo "Second char: ${string[1]}"
+
+
+# 15. WHILE LOOPS
+# ---------------
+echo ''
+echo '=== 15. WHILE LOOPS ==='
+
+n=1
+while [ $n -le 5 ]  # Run while n <= 5
+do
+    echo "Count: $n"
+    n=$(( n + 1 ))
+    # sleep 1  # Uncomment to add 1 second delay
+done
+
+# Alternative syntax:
+# n=1
+# while (( $n <= 5 ))
+# do
+#     echo "$n"
+#     (( n++ ))
+# done
+
+
+# 16. READING FILE LINE BY LINE
+# ------------------------------
+echo ''
+echo '=== 16. READING FILE LINE BY LINE ==='
+
+# Create a temp file for demo
+echo -e "Line 1\nLine 2\nLine 3" > temp_file.txt
+
+# Read file line by line
+while read line
+do
+    echo "Read: $line"
+done < temp_file.txt
+
+# Alternative using cat and pipe:
+cat temp_file.txt | while read line
+do
+    echo "Piped: $line"
+done
+
+rm temp_file.txt  # Clean up
+
+
+# 17. UNTIL LOOPS
+# ---------------
+echo ''
+echo '=== 17. UNTIL LOOPS ==='
+
+n=1
+until [ $n -gt 5 ]  # Run until n > 5
+do
+    echo "Until count: $n"
+    n=$(( n + 1 ))
+done
+
+
+# 18. FOR LOOPS - BASIC
+# ---------------------
+echo ''
+echo '=== 18. FOR LOOPS - BASIC ==='
+
+for num in 1 2 3 4 5
+do
+    echo "Number: $num"
+done
+
+# Range syntax:
+for i in {1..5}
+do
+    echo "Range: $i"
+done
+
+# Range with step:
+for i in {0..10..2}  # 0 to 10, step by 2
+do
+    echo "Step: $i"
+done
+
+
+# 19. FOR LOOPS - COMMANDS
+# ------------------------
+echo ''
+echo '=== 19. FOR LOOPS - COMMANDS ==='
+
+for command in ls pwd date
+do
+    echo "--------------- Running: $command ---------------"
+    $command
+done
+
+
+# 20. FOR LOOPS - FILES/DIRECTORIES
+# ----------------------------------
+echo ''
+echo '=== 20. FOR LOOPS - FILES/DIRECTORIES ==='
+
+echo "Directories in current location:"
+for item in *
+do
+    if [ -d "$item" ]
+    then
+        echo "📁 $item"
+    fi
+done
+
+echo ''
+echo "Files in current location:"
+for item in *
+do
+    if [ -f "$item" ]
+    then
+        echo "📄 $item"
+    fi
+done
+
+
+# ========================================
+# END OF GUIDE
+# ========================================
+echo 'Now go build something cool! 💪'
